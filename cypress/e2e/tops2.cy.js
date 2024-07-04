375636352d448f3facab4c89a22e549fe464b6b6
plbonnier <pl_bonnier@yahoo.fr>
2024-07-04 13:42:21 +0200

    test pour test5

--- cypress/e2e/tops2.cy.js
+++ cypress/e2e/tops2.cy.js
@@ -9,6 +9,7 @@ describe('tops', () => {
         cy.get('img.product-image-photo').first().click();
         cy.url().should('include', 'breathe-easy-tank.html');
         cy.get('[data-ui-id="page-title-wrapper"]').should('contain', 'Breathe-Easy Tank');
+        cy.wait(6000);
         cy.get('#option-label-size-143-item-166').click();
         // cy.get('#option-label-size-143-item-166').should('have.css', 'background-color', 'rgb(255, 255, 255)');
     })
