28c3a22c85dd4171c69157fd7cd22da6918dc566
plbonnier <pl_bonnier@yahoo.fr>
2024-07-04 13:37:37 +0200

    test pour test3

--- cypress/e2e/tops2.cy.js
+++ cypress/e2e/tops2.cy.js
@@ -7,8 +7,8 @@ describe('tops', () => {
         cy.url().should('include', '/women/tops-women.html');
         cy.get('h1.page-title').should('contain', 'Tops');
         cy.get('img.product-image-photo').first().click();
-        // cy.url().should('include', 'breathe-easy-tank.html');
-        // cy.get('[data-ui-id="page-title-wrapper"]').should('contain', 'Breathe-Easy Tank');
+        cy.url().should('include', 'breathe-easy-tank.html');
+        cy.get('[data-ui-id="page-title-wrapper"]').should('contain', 'Breathe-Easy Tank');
         // cy.get('#option-label-size-143-item-166').click();
         // cy.get('#option-label-size-143-item-166').should('have.css', 'background-color', 'rgb(255, 255, 255)');
     })
