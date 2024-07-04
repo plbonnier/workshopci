3382aca24d85f2ca0d07253259145e83e7310d29
plbonnier <pl_bonnier@yahoo.fr>
2024-07-04 16:53:26 +0200

    rapport cypress

--- cypress/e2e/addtocart.cy.js
+++ cypress/e2e/addtocart.cy.js
@@ -1,12 +1,12 @@
 describe('add to cart', () => {
 
     it('add to cart', () => {
-        // cy.visit('https://magento.softwaretestingboard.com/');
-        // cy.get('#ui-id-4').trigger('mouseover');
-        // cy.get('#ui-id-9').click();
-        // cy.get('img.product-image-photo').first().click();
-        // cy.get('#option-label-size-143-item-166').click();
-        // cy.get('#option-label-color-93-item-57').click();
-        // cy.get('#product-addtocart-button').click();
+        cy.visit('https://magento.softwaretestingboard.com/');
+        cy.get('#ui-id-4').trigger('mouseover');
+        cy.get('#ui-id-9').click();
+        cy.get('img.product-image-photo').first().click();
+        cy.get('#option-label-size-143-item-166').click();
+        cy.get('#option-label-color-93-item-57').click();
+        cy.get('#product-addtocart-button').click();
     })
 })
