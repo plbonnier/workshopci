deb3abb7de1dc10821007a3eba14dc2692503ec3
plbonnier <pl_bonnier@yahoo.fr>
2024-07-04 13:32:39 +0200

    test pour test

--- cypress/e2e/tops.cy.js
+++ cypress/e2e/tops.cy.js
@@ -1,17 +1,17 @@
 describe('tops', () => {
 
     it('tops', () => {
-        cy.visit('https://magento.softwaretestingboard.com/');
-        cy.get('#ui-id-4').trigger('mouseover');
-        // cy.get('#ui-id-4').click();
-        cy.get('#ui-id-9').click();
-        // cy.get('.narrow-by-list2 a').first().click();
-        cy.url().should('include', '/women/tops-women.html');
-        cy.get('h1.page-title').should('contain', 'Tops');
-        cy.get('img.product-image-photo').first().click();
-        cy.url().should('include', 'breathe-easy-tank.html');
-        cy.get('[data-ui-id="page-title-wrapper"]').should('contain', 'Breathe-Easy Tank');
-        cy.get('#option-label-size-143-item-166').click();
-        cy.get('#option-label-size-143-item-166').should('have.css', 'background-color', 'rgb(255, 255, 255)');
+        // cy.visit('https://magento.softwaretestingboard.com/');
+        // cy.get('#ui-id-4').trigger('mouseover');
+        // // cy.get('#ui-id-4').click();
+        // cy.get('#ui-id-9').click();
+        // // cy.get('.narrow-by-list2 a').first().click();
+        // cy.url().should('include', '/women/tops-women.html');
+        // cy.get('h1.page-title').should('contain', 'Tops');
+        // cy.get('img.product-image-photo').first().click();
+        // cy.url().should('include', 'breathe-easy-tank.html');
+        // cy.get('[data-ui-id="page-title-wrapper"]').should('contain', 'Breathe-Easy Tank');
+        // cy.get('#option-label-size-143-item-166').click();
+        // cy.get('#option-label-size-143-item-166').should('have.css', 'background-color', 'rgb(255, 255, 255)');
     })
 })
